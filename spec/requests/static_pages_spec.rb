8cfd6862d37f21795b8335a573ffeca83f08f59e
EC2 Default User <ec2-user@ip-172-31-19-203.ap-northeast-1.compute.internal>
2014-10-17 13:05:35 +0000

    3.4

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.configure do |config|
+ config.include Capybara::DSL
+end
+
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_title('Sample App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_title('Ruby on Rails Tutorial Sample App | Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_title('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_title('Ruby on Rails Tutorial Sample App | Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title('About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_title('Ruby on Rails Tutorial Sample App | About Us')
+    end
+  end
+end
+
