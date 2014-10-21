4d280d9d42420af9dedb7b7e25cfde87a167ab8e
EC2 Default User <ec2-user@ip-172-31-19-203.ap-northeast-1.compute.internal>
2014-10-21 06:56:56 +0000

    6.4

--- Gemfile
+++ Gemfile
@@ -27,7 +27,7 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 gem 'spring',        group: :development
 
 # Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
+gem 'bcrypt', '~> 3.1.7'
 
 # Use unicorn as the app server
 # gem 'unicorn'
@@ -42,3 +42,7 @@ gem 'rspec-rails', group: :development
 gem 'capybara', group: :test
 
 gem 'bootstrap-sass', github: 'thomas-mcdonald/bootstrap-sass'
+
+group :development do
+  gem 'annotate', '2.5.0'
+end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,26 +9,19 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_title('Sample App')
-    end
+#    it {page.should have_title('Sample App')}
 
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_title('Ruby on Rails Tutorial Sample App | Home')
-    end
   end
 
   describe "Help page" do
 
     it "should have the h1 'Help'" do
-      visit '/static_pages/help'
+      visit '/help'
       page.should have_title('Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit '/help'
       page.should have_title('Ruby on Rails Tutorial Sample App | Help')
     end
   end
@@ -36,27 +29,27 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the h1 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       page.should have_title('About Us')
     end
 
     it "should have the title 'About Us'" do
-      visit '/static_pages/about'
+      visit '/about'
       page.should have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
   describe "Contact page" do
 
-    it "should have the h1 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact')
-    end
+ #   it "should have the h1 'Contact'" do
+ #     visit '/contact'
+ #     page.should have_selector('h1', text: 'Contact')
+ #   end
 
-    it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title',
-                    text: "Ruby on Rails Tutorial Sample App | Contact")
-    end
+ #   it "should have the title 'Contact'" do
+ #     visit '/contact'
+ #     page.should have_selector('title',
+ #                   text: "Ruby on Rails Tutorial Sample App | Contact")
+  #  end
   end
 end
 
